41cedc7d155d4b0158ed1f2a4244a00e58ab2a9d
YashwardhanChavan <chavanyashwardhan@gmail.com>
2020-11-30 12:13:52 +0530

    Added LoginActivity and RegisterActivity with default android theme. User authentication and firebase stuff remaining.

--- app/src/main/java/com/example/trekkin/RegisterActivity.java
+++ app/src/main/java/com/example/trekkin/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.trekkin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        setTitle("Registration");
+    }
+
+    public void launchLoginActivity(View view) {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
