a867123c2721c5cf290c8c7ddef9ad1d6c0345c9
YashwardhanChavan <chavanyashwardhan@gmail.com>
2020-12-01 12:02:59 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts[RESOLVED]:
    #	app/src/main/res/values/strings.xml
    
    Added date picker dialog for choosing date of birth.

--- app/src/main/java/com/example/trekkin/RegisterActivity.java
+++ app/src/main/java/com/example/trekkin/RegisterActivity.java
@@ -2,17 +2,52 @@ package com.example.trekkin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText mEditText;
+    private DatePickerDialog.OnDateSetListener mOnDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         setTitle("Registration");
+
+        mEditText = findViewById(R.id.edittext_dob_registration_activity);
+
+        mEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(RegisterActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mOnDateSetListener, year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mOnDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String date = year + "/" + month + "/" + dayOfMonth;
+                mEditText.setText(date);
+            }
+        };
     }
 
     public void launchLoginActivity(View view) {
